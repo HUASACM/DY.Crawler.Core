10d1cd6d124411e9fd1d4fce526a24e7784a230a
wujunyi <632075803@qq.com>
2017-11-22 17:08:57 +0800

    增加string扩展方法, 简化get过程

--- Domains/Extensions/EnumerableExtensions.cs
+++ Domains/Extensions/EnumerableExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,6 +20,11 @@ namespace DY.Crawler.Core.Domains.Extensions
         public static IEnumerable<int> to(this int start, int end)
         {
             for (int i = start; i <= end; i++) yield return i;
+        }
+
+        public static string get_content(this string url, HttpClient client)
+        {
+            return client.GetStringAsync(url).Result;
         }
     }
 }
